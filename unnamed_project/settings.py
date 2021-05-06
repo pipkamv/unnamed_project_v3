6c48c81016103c886ac5024ece29d3a9c8934e31
My name <iakylbek005@gmail.com>
2021-05-06 17:37:59 +0600

    novayaidregistration

--- unnamed_project/settings.py
+++ unnamed_project/settings.py
@@ -189,12 +189,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # django_heroku.settings(locals())
 
-
+# 7355608novaya
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'alex.web.developer.kg@gmail.com'  # alex.web.developer.kg@gmail.com
-EMAIL_HOST_PASSWORD = 'ffzrsonqhiwrmoha'  # config('EMAIL_HOST_PASSWORD')ffzrsonqhiwrmoha
+EMAIL_HOST_USER = 'novayanormaid@gmail.com'# alex.web.developer.kg@gmail.com
+EMAIL_HOST_PASSWORD = '7355608novaya'  # config('EMAIL_HOST_PASSWORD')ffzrsonqhiwrmoha
 EMAIL_PORT = 587
 
 CORS_REPLACE_HTTPS_REFERER      = False
