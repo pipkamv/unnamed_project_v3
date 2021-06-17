173830fe0ddbb75c685389c8a305b92e32a7a677
My name <iakylbek005@gmail.com>
2021-06-17 16:01:35 +0600

    fix big

--- users/views.py
+++ users/views.py
@@ -155,7 +155,6 @@ class SetNewPasswordAPIView(generics.GenericAPIView):
     serializer_class = SetNewPasswordSerializer
 
     def patch(self, request):
-        print(request.data)
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
         return Response({'success': True, 'message': 'Password reset success'}, status=status.HTTP_200_OK)
